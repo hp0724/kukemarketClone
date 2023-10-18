5b7d33f29bec29ecef66bdc9e725f1bc04ab9c18
suha <hp980724@gmail.com>
2023-10-19 01:01:21 +0900

    feat : sign service 추가 및 테스트

---
+++ src/test/java/kukekyakya/kukemarket/service/sign/TokenServiceTest.java
@@ -0,0 +1,61 @@
+package kukekyakya.kukemarket.service.sign;
+
+import kukekyakya.kukemarket.handler.JwtHandler;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class TokenServiceTest {
+    //의존성을 가지고 있는 객체들을 가짜로 만들어서 주입받을 수 있도록 함
+    @InjectMocks TokenService tokenService;
+    //객체들을 가짜로 만들어서 @InjectMocks로 지정된 객체에 주입
+    @Mock
+    JwtHandler jwtHandler;
+
+    @BeforeEach
+    void beforeEach(){
+        //TokenService는 @Value를 이용하여 설정 파일에서 값을 읽어와야합니다
+        //리플렉션을 이용해서 어떠한 객체의 필드 값을 임의의 값으로 주입
+        ReflectionTestUtils.setField(tokenService, "accessTokenMaxAgeSeconds", 10L);
+        ReflectionTestUtils.setField(tokenService, "refreshTokenMaxAgeSeconds", 10L);
+        ReflectionTestUtils.setField(tokenService, "accessKey", "accessKey");
+        ReflectionTestUtils.setField(tokenService, "refreshKey", "refreshKey");
+    }
+
+    @Test
+    void createAccessTokenTest() {
+        // given
+        given(jwtHandler.createToken(anyString(), anyString(), anyLong())).willReturn("access");
+
+        // when
+        String token = tokenService.createAccessToken("subject");
+
+        // then
+        assertThat(token).isEqualTo("access");
+        verify(jwtHandler).createToken(anyString(), anyString(), anyLong());
+    }
+
+    @Test
+    void createRefreshTokenTest() {
+        // given
+        given(jwtHandler.createToken(anyString(), anyString(), anyLong())).willReturn("refresh");
+
+        // when
+        String token = tokenService.createRefreshToken("subject");
+
+        // then
+        assertThat(token).isEqualTo("refresh");
+        verify(jwtHandler).createToken(anyString(), anyString(), anyLong());
+    }
+}

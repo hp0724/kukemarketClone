a12f4a85359d5e87725d2b52668fe7a23057cf53
suha <hp980724@gmail.com>
2023-11-01 00:29:36 +0900

    스프링부트 게시판 API 서버 만들기 (7) - detached entity passed to persist 해결하기

--- src/main/java/kukekyakya/kukemarket/InitDB.java
+++ src/main/java/kukekyakya/kukemarket/InitDB.java
@@ -1,14 +1,20 @@
 package kukekyakya.kukemarket;
 
+import kukekyakya.kukemarket.entity.member.Member;
 import kukekyakya.kukemarket.entity.member.Role;
 import kukekyakya.kukemarket.entity.member.RoleType;
+import kukekyakya.kukemarket.exception.RoleNotFoundException;
+import kukekyakya.kukemarket.repository.member.MemberRepository;
 import kukekyakya.kukemarket.repository.role.RoleRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Profile;
+import org.springframework.context.event.EventListener;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.annotation.PostConstruct;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -19,16 +25,43 @@ import java.util.stream.Collectors;
 @Profile("local")
 public class InitDB {
     private final RoleRepository roleRepository;
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    //빈의 생성과 의존성 주입이 끝난뒤에 초기화
-    @PostConstruct
+
+
+    //모든 준비가 완료되었을때 발생 하는 이벤트
+    @EventListener(ApplicationReadyEvent.class)
+    @Transactional
     public void initDB(){
         log.info("initialize database");
         initRole();
+        initTestAdmin();
+        initTestMember();
     }
     private void initRole(){
         roleRepository.saveAll(
                 List.of(RoleType.values()).stream().map(roleType -> new Role(roleType)).collect(Collectors.toList()));
+
+    }
+
+    private void initTestAdmin(){
+        memberRepository.save(
+                new Member("admin@admin.com",passwordEncoder.encode("123456a!"),"admin","admin",
+                        List.of(roleRepository.findByRoleType(RoleType.ROLE_NORMAL).orElseThrow(RoleNotFoundException::new),
+                                roleRepository.findByRoleType(RoleType.ROLE_ADMIN).orElseThrow(RoleNotFoundException::new)))
+
+        );
+    }
+
+    private void initTestMember(){
+        memberRepository.saveAll(
+                List.of(
+                        new Member("member1@member.com", passwordEncoder.encode("123456a!"), "member1", "member1",
+                                List.of(roleRepository.findByRoleType(RoleType.ROLE_NORMAL).orElseThrow(RoleNotFoundException::new))),
+                        new Member("member2@member.com", passwordEncoder.encode("123456a!"), "member2", "member2",
+                                List.of(roleRepository.findByRoleType(RoleType.ROLE_NORMAL).orElseThrow(RoleNotFoundException::new))))
+                );
 
     }
 }
